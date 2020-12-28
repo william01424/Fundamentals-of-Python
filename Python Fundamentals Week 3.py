dde30487b5fa1cdb7b27be9cb06781b6f3f94c06
Will Templeton <75176144+william01424@users.noreply.github.com>
2020-12-28 19:17:19 +0000

    First half of Lecture
    Includes import OS as well as typical commands to navigate files and directories in Windows PowerShell.

---
+++ Python Fundamentals Week 3.py
@@ -0,0 +1,14 @@
+## The File System with Python
+
+import os
+print()
+print(os.getcwd())      # Not sure why this is not printing anything?
+print("The separator on this computer is: ", os.sep)
+
+# How to navigate the file system through PowerShell (Windows)
+
+# Commands run in windows powershell include basic ls ,cd, creating files and removed them (similar with directories)
+# In order to save multiple lines for space I will not include this here - please see lecture notes for details
+# Windows and Mac is avail!
+
+# Writing and reading files with Python
