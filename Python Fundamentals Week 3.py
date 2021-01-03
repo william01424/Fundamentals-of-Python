8cdc42f306818c49f0995fd4e612ed1da8896db4
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-01-03 20:17:35 +0000

    Update Python Fundamentals Week 3.py

--- Python Fundamentals Week 3.py
+++ Python Fundamentals Week 3.py
@@ -9,6 +9,11 @@ print("The separator on this computer is: ", os.sep)
 
 # Commands run in windows powershell include basic ls ,cd, creating files and removed them (similar with directories)
 # In order to save multiple lines for space I will not include this here - please see lecture notes for details
-# Windows and Mac is avail!
+
+
+# Windows and Mac is available in the video - see black background for windows PWSh and light for MacOS. I wont be looking at mac OS as windows is my preffered OS
+
 
 # Writing and reading files with Python
+
+
